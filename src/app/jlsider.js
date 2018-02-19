a19bface34d8302ebe8cc1c1ab38c4305f5574a7
julianleahy <julianleahynz@gmail.com>
2018-02-20 11:19:43 +1300

    define slider vars

--- src/app/jlsider.js
+++ src/app/jlsider.js
@@ -1,3 +1,20 @@
 import 'styles/jlslider.scss'
 
-export default () => {}
+export default () => {
+
+    const
+        slider = $('.jlsliderPanel'),
+        slide = $('.jlslide'),
+        numSlides = slide.length,
+        autoPlaySpeed = 5000,
+        transitionSpeed = 500;
+
+    let
+        windowWidth = $(window).width(),
+        panelWidth = (numSlides * windowWidth) + 'px',
+        timerAutoPlay = null,
+        slideCount = 1,
+        animateTo = 'next';
+
+
+}
