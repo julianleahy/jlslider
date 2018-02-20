701c8b41a86f94d343e61a501610fa59e2bc867c
julianleahy <julianleahynz@gmail.com>
2018-02-20 19:12:14 +1300

    update config to fix bug in safari not displaying bg imgs

--- webpack.config.js
+++ webpack.config.js
@@ -31,30 +31,12 @@ module.exports = {
             {
                 test: /\.(gif|png|jpe?g|svg)$/i,
                 use: [
-                    'file-loader?name=[path][name].[ext]',
+                    'file-loader',
                     {
                         loader: 'image-webpack-loader',
                         options: {
-                            mozjpeg: {
-                                progressive: true,
-                                quality: 80
-                            },
-                            // optipng.enabled: false will disable optipng
-                            optipng: {
-                                enabled: false,
-                            },
-                            pngquant: {
-                                quality: '65-90',
-                                speed: 4
-                            },
-                            gifsicle: {
-                                interlaced: false,
-                            },
-                            // the webp option will enable WEBP
-                            webp: {
-                                quality: 75
-                            }
-                        }
+                            bypassOnDebug: true,
+                        },
                     },
                 ],
             }
@@ -93,5 +75,5 @@ module.exports = {
         }),
     ]
 
-    
+
 }
