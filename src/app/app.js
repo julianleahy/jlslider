702c19841c73a92271a6dd9105ca6ce3113afc18
julianleahy <julianleahynz@gmail.com>
2018-03-14 11:37:28 +1300

    include preloader styles and add function to load

--- src/app/app.js
+++ src/app/app.js
@@ -1,9 +1,15 @@
 import 'styles/normalize.css';
 import 'styles/main.scss';
+import 'styles/preloader.scss';
 
 import JLSlider from './jlsider';
 
 $(function() {
+  // can remove setTimeout just for demo
+  setTimeout(function(){
+    $('body').addClass('loaded');
     JLSlider();
+	}, 1500);
+    
 });
 
